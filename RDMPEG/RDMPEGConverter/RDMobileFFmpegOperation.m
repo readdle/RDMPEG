8c9247c37be6b20a0bb60b3435dfa3abb605b390
Artem <>
2020-08-27 12:17:56 +0300

    fixed ffmpeg delegate

--- RDMPEG/RDMPEGConverter/RDMobileFFmpegOperation.m
+++ RDMPEG/RDMPEGConverter/RDMobileFFmpegOperation.m
@@ -12,7 +12,30 @@
 #include <libavformat/avformat.h>
 #import <Log4Cocoa/Log4Cocoa.h>
 
-@interface RDMobileFFmpegOperation()<StatisticsDelegate,LogDelegate>
+
+
+@protocol RDMobileFFmpegStatisticsDelegate <NSObject>
+
+- (void)statisticsCallback:(Statistics *)statistics;
+
+@end
+
+
+
+@interface RDMobileFFmpegDelegate : NSObject <StatisticsDelegate,LogDelegate>
+
+@property (nonatomic, strong)NSHashTable <RDMobileFFmpegStatisticsDelegate> *statisticsDelegates;
+
++ (instancetype)sharedDelegate;
+
+- (void)addStatisticsDelegate:(id<RDMobileFFmpegStatisticsDelegate>)statisticsDelegate;
+
+- (void)removeStatisticsDelegate:(id<RDMobileFFmpegStatisticsDelegate>)statisticsDelegate;
+
+@end
+
+
+@interface RDMobileFFmpegOperation()<RDMobileFFmpegStatisticsDelegate>
 
 @property (nonatomic,strong)NSArray<NSString *> *arguments;
 @property (nonatomic,copy)RDMobileFFmpegOperationResultBlock resultBlock;
@@ -32,9 +55,7 @@
     }
     self = [super init];
     if(self){
-        [MobileFFmpegConfig setStatisticsDelegate:self];
-        [MobileFFmpegConfig setLogDelegate:self];
-        [MobileFFmpegConfig enableRedirection];
+        [[RDMobileFFmpegDelegate sharedDelegate] addStatisticsDelegate:self];
         self.arguments = arguments;
         self.resultBlock = resultBlock;
         self.statisticsBlock = statisticsBlock;
@@ -42,8 +63,8 @@
     return self;
 }
 
-- (void)logCallback:(long)executionId :(int)level :(NSString*)message{
-    log4Debug(@"level: %@, message: %@",@(level),message);
+- (void)dealloc{
+    [[RDMobileFFmpegDelegate sharedDelegate] removeStatisticsDelegate:self];
 }
 
 - (void)statisticsCallback:(Statistics *)statistics{
@@ -71,6 +92,62 @@
 
 + (BOOL)isReturnCodeSuccess:(int)code{
     return code == RETURN_CODE_SUCCESS;
+}
+
+@end
+
+
+@implementation RDMobileFFmpegDelegate
+
++ (instancetype)sharedDelegate{
+    static dispatch_once_t onceToken;
+    static RDMobileFFmpegDelegate *sharedDelegate;
+    dispatch_once(&onceToken, ^{
+        sharedDelegate = [RDMobileFFmpegDelegate new];
+    });
+    return sharedDelegate;
+}
+
+- (instancetype)init{
+    self = [super init];
+    if(self){
+        [MobileFFmpegConfig setStatisticsDelegate:self];
+        [MobileFFmpegConfig setLogDelegate:self];
+        [MobileFFmpegConfig enableRedirection];
+        self.statisticsDelegates = (NSHashTable <RDMobileFFmpegStatisticsDelegate> *)[NSHashTable weakObjectsHashTable];
+    }
+    return self;
+}
+
+- (void)logCallback:(long)executionId :(int)level :(NSString*)message{
+    log4Debug(@"level: %@, message: %@",@(level),message);
+}
+
+- (void)statisticsCallback:(Statistics *)statistics{
+    @synchronized (self.statisticsDelegates) {
+        NSArray *delegates = [self.statisticsDelegates allObjects];
+        for (id<RDMobileFFmpegStatisticsDelegate> delegate in delegates) {
+            [delegate statisticsCallback:statistics];
+        }
+    }
+}
+
+- (void)addStatisticsDelegate:(id<RDMobileFFmpegStatisticsDelegate>)statisticsDelegate{
+    if(statisticsDelegate == nil){
+        return;
+    }
+    @synchronized (self.statisticsDelegates) {
+        [self.statisticsDelegates addObject:statisticsDelegate];
+    }
+}
+
+- (void)removeStatisticsDelegate:(id<RDMobileFFmpegStatisticsDelegate>)statisticsDelegate{
+    if(statisticsDelegate == nil){
+        return;
+    }
+    @synchronized (self.statisticsDelegates) {
+        [self.statisticsDelegates removeObject:statisticsDelegate];
+    }
 }
 
 @end
