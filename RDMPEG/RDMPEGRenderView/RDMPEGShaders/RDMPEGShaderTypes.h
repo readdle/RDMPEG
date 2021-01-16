52d0259df4e6f3fa1035a018d4f97963dc208db5
Serhii Alpieiev <s.alpieiev@readdle.com>
2021-01-16 17:27:11 +0200

    RDMPEGShaderTypes cleanup

--- RDMPEG/RDMPEGRenderView/RDMPEGShaders/RDMPEGShaderTypes.h
+++ RDMPEG/RDMPEGRenderView/RDMPEGShaders/RDMPEGShaderTypes.h
@@ -9,21 +9,19 @@
 #ifndef RDMPEGShaderTypes_h
 #define RDMPEGShaderTypes_h
 
+
 #include <simd/simd.h>
 
-// Buffer index values shared between shader and C code to ensure Metal shader buffer inputs match
-//   Metal API buffer set calls
 typedef enum RDMPEGVertexInputIndex {
     RDMPEGVertexInputIndexVertices,
     RDMPEGVertexInputIndexViewportSize,
 } RDMPEGVertexInputIndex;
 
-// Texture index values shared between shader and C code to ensure Metal shader buffer inputs match
-//   Metal API texture set calls
 
 typedef enum RDMPEGTextureIndexBGRA {
     RDMPEGTextureIndexBGRABaseColor,
 } RDMPEGTextureIndexBGRA;
+
 
 typedef enum RDMPEGTextureIndexYUV {
     RDMPEGTextureIndexY,
@@ -31,16 +29,9 @@ typedef enum RDMPEGTextureIndexYUV {
     RDMPEGTextureIndexV,
 } RDMPEGTextureIndexYUV;
 
-//  This structure defines the layout of each vertex in the array of vertices set as an input to the
-//    Metal vertex shader.  Since this header is shared between the .metal shader and C code,
-//    you can be sure that the layout of the vertex array in the code matches the layout that
-//    the vertex shader expects
 
 typedef struct {
-    // Positions in pixel space. A value of 100 indicates 100 pixels from the origin/center.
     vector_float2 position;
-
-    // 2D texture coordinate
     vector_float2 textureCoordinate;
 } RDMPEGVertex;
 
