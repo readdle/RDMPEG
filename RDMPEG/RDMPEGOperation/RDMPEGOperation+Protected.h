99cad3b0c1b64977462126544e1ba008b039af47
Serhii Alpieiev <s.alpieiev@readdle.com>
2021-08-28 00:49:03 +0300

    Updated to FFmpegKit

---
+++ RDMPEG/RDMPEGOperation/RDMPEGOperation+Protected.h
@@ -0,0 +1,21 @@
+//
+//  RDMPEGOperation+Protected.h
+//  RDMPEG
+//
+//  Created by Serhii Alpieiev on 27.08.2021.
+//  Copyright © 2021 Readdle. All rights reserved.
+//
+
+#import "RDMPEGOperation.h"
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RDMPEGOperation (Protected)
+
+- (void)completeOperation;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RDMPEG/RDMPEGOperation/RDMPEGOperation.h
@@ -0,0 +1,19 @@
+//
+//  RDMPEGOperation.h
+//  RDMPEG
+//
+//  Created by Serhii Alpieiev on 27.08.2021.
+//  Copyright © 2021 Readdle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RDMPEGOperation : NSOperation
+
+@end
+
+NS_ASSUME_NONNULL_END
