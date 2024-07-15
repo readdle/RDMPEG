2433c15175ca9b2f8972a18a7bdfa9a4c5663adb
Maksym Berezhnyi <mberezhnyi@readdle.com>
2024-07-15 16:12:44 +0200

    Converted subtitle parser

--- RDMPEG/RDMPEGDecoder/RDMPEGDecoder.m
+++ RDMPEG/RDMPEGDecoder/RDMPEGDecoder.m
@@ -8,7 +8,6 @@
 
 #import "RDMPEGDecoder.h"
 #import "RDMPEGIOStream.h"
-#import "RDMPEGSubtitleASSParser.h"
 #import "RDMPEGFrames+Decoder.h"
 #import "RDMPEGStream+Decoder.h"
 #import <Accelerate/Accelerate.h>
@@ -22,7 +21,7 @@
 #import <libavfilter/buffersrc.h>
 #import <Log4Cocoa/Log4Cocoa.h>
 
-
+#import <RDMPEG/RDMPEG-Swift.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
