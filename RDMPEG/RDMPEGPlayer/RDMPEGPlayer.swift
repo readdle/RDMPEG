1cec777e32e2f840b2006a7d0e86b9e17580107a
Denys Litvinskyi <dlitvinskyi@readdle.com>
2025-07-29 11:58:16 +0300

    Revert "DOC-14148: Limit maximum size of the audio buffer"
    This reverts commit cfac7d69eb2e31165c959fd72c696fb403855dee.

--- RDMPEG/RDMPEGPlayer/RDMPEGPlayer.swift
+++ RDMPEG/RDMPEGPlayer/RDMPEGPlayer.swift
@@ -13,7 +13,6 @@ import ReaddleLib
 private let RDMPEGPlayerMinVideoBufferSize: TimeInterval = 0.2
 private let RDMPEGPlayerMaxVideoBufferSize: TimeInterval = 1.0
 private let RDMPEGPlayerMinAudioBufferSize: TimeInterval = 0.2
-private let RDMPEGPlayerMaxAudioBufferSize: TimeInterval = 1.0
 
 private let RDMPEGPlayerInputDecoderKey = "RDMPEGPlayerInputDecoderKey"
 private let RDMPEGPlayerInputNameKey = "RDMPEGPlayerInputNameKey"
@@ -1202,10 +1201,6 @@ public class RDMPEGPlayer: NSObject {
                     return true
                 }
 
-                if framebuffer.bufferedAudioDuration >= RDMPEGPlayerMaxAudioBufferSize {
-                    return true
-                }
-
                 if framebuffer.bufferedAudioDuration > RDMPEGPlayerMinAudioBufferSize {
                     return true
                 }
@@ -1221,10 +1216,6 @@ public class RDMPEGPlayer: NSObject {
                     return true
                 }
 
-                if framebuffer.bufferedAudioDuration >= RDMPEGPlayerMaxAudioBufferSize {
-                    return true
-                }
-
                 if framebuffer.bufferedAudioDuration > RDMPEGPlayerMinAudioBufferSize {
                     return true
                 }
@@ -1237,10 +1228,6 @@ public class RDMPEGPlayer: NSObject {
         }
         else {
             guard let decoder = decoder, decoder.isAudioStreamExist, decoder.isEndReached == false else {
-                return true
-            }
-
-            if framebuffer.bufferedAudioDuration >= RDMPEGPlayerMaxAudioBufferSize {
                 return true
             }
 
